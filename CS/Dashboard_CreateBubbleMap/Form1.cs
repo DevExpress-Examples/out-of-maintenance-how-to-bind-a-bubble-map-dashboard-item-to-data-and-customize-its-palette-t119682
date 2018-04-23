298380b72d29ed49bf3a7cc8fd8ce6a9a8d81ce0
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Dashboard_CreateBubbleMap/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using DevExpress.DashboardCommon;
+using DevExpress.DataAccess.ConnectionParameters;
+using DevExpress.DataAccess.Sql;
+using DevExpress.XtraEditors;
+
+namespace Dashboard_CreateBubbleMap {
+    public partial class Form1 : XtraForm {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // Creates a new dashboard and a data source for this dashboard.
+            Dashboard dashboard = new Dashboard();
+            DashboardSqlDataSource dataSource = new DashboardSqlDataSource(); 
+            dataSource.ConnectionParameters =
+                new XmlFileConnectionParameters(@"..\..\Data\DashboardEnergyStatictics.xml");
+            SelectQuery sqlQuery = SelectQueryFluentBuilder
+                .AddTable("Countries")
+                .SelectColumns("Latitude", "Longitude", "Production", "Import", "Country")
+                .Build("Countries");
+
+            dataSource.Queries.Add(sqlQuery);
+            dashboard.DataSources.Add(dataSource);
+
+            // Creates a Bubble Map dashboard item and specifies its data source.
+            BubbleMapDashboardItem bubbleMap = new BubbleMapDashboardItem();
+            bubbleMap.DataSource = dashboard.DataSources[0];
+            bubbleMap.DataMember = "Countries";
+
+            // Loads the map of the world.
+            bubbleMap.Area = ShapefileArea.Europe;
+
+            // Provides cities' coordinates.
+            bubbleMap.Latitude = new Dimension("Latitude");
+            bubbleMap.Longitude = new Dimension("Longitude");
+
+            // Specifies measures used to evaluate bubble weights and colors.
+            bubbleMap.Weight = new Measure("Production", SummaryType.Sum);
+            bubbleMap.Color = new Measure("Import", SummaryType.Sum); 
+            bubbleMap.Color.NumericFormat.FormatType = DataItemNumericFormatType.General;
+
+            // Provides additional values displayed within bubble tooltips.
+            bubbleMap.TooltipDimensions.Add(new Dimension("Country"));
+
+            // Specifies a custom scale and shows map legends.
+            CustomizeScale(bubbleMap);
+            ShowLegends(bubbleMap);
+
+            // Adds the Bubble Map dashboard item to the dashboard and opens this
+            // dashboard in the Dashboard Viewer.
+            dashboard.Items.Add(bubbleMap);
+            dashboardViewer1.Dashboard = dashboard;
+        }
+
+        private void CustomizeScale(BubbleMapDashboardItem map) {
+            CustomScale customScale = new CustomScale();
+            List<double> rangeStops = new List<double>();
+
+            // Specifies that the absolute scale is used to define a set of range stops.
+            customScale.IsPercent = false;
+
+            // Specifies custom range stops.   
+            rangeStops.Add(20);
+            rangeStops.Add(200);
+            rangeStops.Add(500);
+            rangeStops.Add(2000);            
+
+            // Adds custom range stops to a custom scale.  
+            customScale.RangeStops.AddRange(rangeStops);
+
+            map.ColorScale = customScale;
+        }
+
+        private void ShowLegends(BubbleMapDashboardItem map) {
+            // Enables map legends and specifies its position and orientation.
+            map.Legend.Visible = true;
+            map.Legend.Orientation = MapLegendOrientation.Horizontal;
+            map.WeightedLegend.Visible = true;
+            map.WeightedLegend.Position = MapLegendPosition.BottomLeft;
+        }
+    }
+}
---
+++ VB/Dashboard_CreateBubbleMap/Form1.vb
@@ -0,0 +1,86 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports DevExpress.DashboardCommon
+Imports DevExpress.DataAccess.ConnectionParameters
+Imports DevExpress.DataAccess.Sql
+Imports DevExpress.XtraEditors
+
+Namespace Dashboard_CreateBubbleMap
+    Partial Public Class Form1
+        Inherits XtraForm
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+            ' Creates a new dashboard and a data source for this dashboard.
+            Dim dashboard As New Dashboard()
+            Dim dataSource As New DashboardSqlDataSource()
+            dataSource.ConnectionParameters =
+                New XmlFileConnectionParameters("..\..\Data\DashboardEnergyStatictics.xml")
+            Dim sqlQuery As SelectQuery = SelectQueryFluentBuilder.AddTable("Countries") _
+                                          .SelectColumns("Latitude", "Longitude", "Production", "Import", "Country") _
+                                          .Build("Countries")
+
+            dataSource.Queries.Add(sqlQuery)
+            dashboard.DataSources.Add(dataSource)
+
+            ' Creates a Bubble Map dashboard item and specifies its data source.
+            Dim bubbleMap As New BubbleMapDashboardItem()
+            bubbleMap.DataSource = dashboard.DataSources(0)
+            bubbleMap.DataMember = "Countries"
+
+            ' Loads the map of the world.
+            bubbleMap.Area = ShapefileArea.Europe
+
+            ' Provides cities' coordinates.
+            bubbleMap.Latitude = New Dimension("Latitude")
+            bubbleMap.Longitude = New Dimension("Longitude")
+
+            ' Specifies measures used to evaluate bubble weights and colors.
+            bubbleMap.Weight = New Measure("Production", SummaryType.Sum)
+            bubbleMap.Color = New Measure("Import", SummaryType.Sum)
+            bubbleMap.Color.NumericFormat.FormatType = DataItemNumericFormatType.General
+
+            ' Provides additional values displayed within bubble tooltips.
+            bubbleMap.TooltipDimensions.Add(New Dimension("Country"))
+
+            ' Specifies a custom scale and shows map legends.
+            CustomizeScale(bubbleMap)
+            ShowLegends(bubbleMap)
+
+            ' Adds the Bubble Map dashboard item to the dashboard and opens this
+            ' dashboard in the Dashboard Viewer.
+            dashboard.Items.Add(bubbleMap)
+            dashboardViewer1.Dashboard = dashboard
+        End Sub
+
+        Private Sub CustomizeScale(ByVal map As BubbleMapDashboardItem)
+            Dim customScale As New CustomScale()
+            Dim rangeStops As New List(Of Double)()
+
+            ' Specifies that the absolute scale is used to define a set of range stops.
+            customScale.IsPercent = False
+
+            ' Specifies custom range stops.   
+            rangeStops.Add(20)
+            rangeStops.Add(200)
+            rangeStops.Add(500)
+            rangeStops.Add(2000)
+
+            ' Adds custom range stops to a custom scale.  
+            customScale.RangeStops.AddRange(rangeStops)
+
+            map.ColorScale = customScale
+        End Sub
+
+        Private Sub ShowLegends(ByVal map As BubbleMapDashboardItem)
+            ' Enables map legends and specifies its position and orientation.
+            map.Legend.Visible = True
+            map.Legend.Orientation = MapLegendOrientation.Horizontal
+            map.WeightedLegend.Visible = True
+            map.WeightedLegend.Position = MapLegendPosition.BottomLeft
+        End Sub
+    End Class
+End Namespace
